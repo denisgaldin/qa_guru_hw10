4974891c567593d10b6dc9a5922bd18aa7c8dfdc
denisgaldin <denisgaldin@mail.ru>
2024-10-22 17:25:11 +0300

    Изменение кода

--- tests/test_github_simple.py
+++ tests/test_github_simple.py
@@ -6,9 +6,9 @@ def test_github_simple():
     browser.open('https://github.com')
 
     browser.element('.header-search-button').click()
-    browser.element('#query-builder-test').type('denisgaldin/qa_guru_hw10').press_enter()
-    browser.element(by.link_text('denisgaldin/qa_guru_hw10')).click()
+    browser.element('#query-builder-test').type('eroshenkoam/allure - example').press_enter()
+    browser.element(by.link_text('eroshenkoam/allure-example')).click()
     browser.element('#issues-tab').click()
-    browser.element(by.text('Issue for qa_guru_hw10')).click()
+    browser.element(by.text('Issue for HW qa.guru')).click()
 
-    browser.element('.gh-header-title').should(have.text('Issue for qa_guru_hw10'))
+    browser.element('.gh-header-title').should(have.text('Issue for HW qa.guru'))
--- tests/test_labda.py
+++ tests/test_labda.py
@@ -17,8 +17,8 @@ def test_github_lambda():
 
     with allure.step('Перейти в репозиторий allure-example'):
         browser.element('.header-search-button').click()
-        browser.element('#query-builder-test').type('denisgaldin / allure - example').press_enter()
-        browser.element(by.link_text('denisgaldin/allure-example')).click()
+        browser.element('#query-builder-test').type('eroshenkoam / allure - example').press_enter()
+        browser.element(by.link_text('eroshenkoam/allure-example')).click()
 
     with allure.step('Проверить раздел Issue for HW qa.guru'):
         browser.element('#issues-tab').click()
